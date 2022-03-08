b11e5f4041506767387f9760249590af6c3a33fc
Sergei Sergeev <68876374+SergeevSergeiS@users.noreply.github.com>
2022-03-08 21:51:24 +0400

    Create Main.java

---
+++ Minesweeper/Main.java
@@ -0,0 +1,8 @@
+package minesweeper;
+
+public class Main {
+    public static void main(String[] args) {
+        MinesweeperBoard board = new MinesweeperBoard();
+        board.playGame();
+    }
+}
