123314e99eb06af5b85719d4f178b34b62e9f4bb
Sergei Sergeev <68876374+SergeevSergeiS@users.noreply.github.com>
2022-04-10 00:18:36 +0400

    Create SerialiseUtil.java

---
+++ BlockChain/SerialiseUtil.java
@@ -0,0 +1,23 @@
+package blockchain;
+
+import java.io.*;
+
+public class SerialiseUtil {
+    public static void serialize(Object obj, String fileName) throws IOException {
+        FileOutputStream fos = new FileOutputStream(fileName);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(obj);
+        oos.close();
+    }
+
+    public static Object deserialize(String fileName) throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        Object obj = ois.readObject();
+        ois.close();
+
+        return obj;
+    }
+}
