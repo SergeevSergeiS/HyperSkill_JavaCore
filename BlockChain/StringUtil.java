cf920165a74456f5cdb407cd75f1ae4085a68f38
Sergei Sergeev <68876374+SergeevSergeiS@users.noreply.github.com>
2022-04-10 00:19:03 +0400

    Create StringUtil.java

---
+++ BlockChain/StringUtil.java
@@ -0,0 +1,27 @@
+package blockchain;
+
+import java.security.MessageDigest;
+
+class StringUtil {
+    public static String applySha256(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte elem : hash) {
+                String hex = Integer.toHexString(0xff & elem);
+
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
